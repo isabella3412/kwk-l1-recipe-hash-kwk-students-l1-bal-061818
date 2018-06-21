b351bff242682612047683615c477376ae7d13b2
isabella3412 <izzybella17@icloud.com>
2018-06-21 18:57:03 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -25,13 +25,6 @@ perfect_10_recipe = {
   :agave => "1/4 cup"
 }
 
-
-
-
-
-
-
-
 #2. Write a method that simply returns (not puts) your perfect_10 hash
 def recipe_ingredients(perfect_10_recipe)
   perfect_10_recipe
