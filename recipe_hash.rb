faf3f4789835ffd3345552c72beb7a3ab45fe11f
isabella3412 <izzybella17@icloud.com>
2018-06-21 18:12:52 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -17,11 +17,11 @@ perfect_10_recipe = {
   :gluten_fleewhole_oats => "1 cup",
   :kosher_salt => "1/2 tea spoon",
   :baking_powder => "1/2 tea spoon",
-  :baking_soda  => "1/4 tea spoon"
-  :Xanthan_gum => "1/2 tea spoon"
-  :slivered_almonds_lightly_toasted => "1/4 cup"
-  :mini_dark_chocolate_chips => "3/4 cup"
-  :olive_oil => "1/2 cup"
+  :baking_soda  => "1/4 tea spoon",
+  :Xanthan_gum => "1/2 tea spoon",
+  :slivered_almonds_lightly_toasted => "1/4 cup",
+  :mini_dark_chocolate_chips => "3/4 cup",
+  :olive_oil => "1/2 cup",
   :agave => "1/4 cup"
 }
 
